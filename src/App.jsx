1178338ab3a27a0b2c167b907e12ef02f6fa0e6c
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-22 10:05:50 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// @flow strict
+
+function App() {
+    return (
+        <div className="mt-10">
+            <h3 className="text-center font-extrabold text-4xl text-purple-700">this is my first redux setup</h3>
+        </div>
+    );
+}
+
+export default App;
